1cfd7a95ef98f111ec179b1198e4d94a6ed4f4f3
Simon Rüegg <simon@rueggs.ch>
2016-09-07 20:04:40 -0400

    Assignment 1

---
+++ programm1.pl
@@ -0,0 +1,42 @@
+% Assignment 1
+% Author Simon Rüegg
+
+% daughters
+female(ann).
+female(lis).
+
+% son
+male(bill).
+
+% mother
+female(marie).
+
+% orphan
+female(alice).
+
+% grandmother
+female(grace).
+
+% fahter
+male(joe).
+
+% grandfather
+male(jason).
+
+parent_of(ann, marie).
+parent_of(ann, joe).
+patent_of(lis, marie).
+parent_of(lis, joe).
+parent_of(bill, joe).
+parent_of(bill, marie).
+
+parent_of(joe, jason).
+parent_of(joe, grace).
+
+sibling_of(X, Y) :- parent_of(X, Z), parent_of(Y, Z).
+
+father_of(X, Y) :- male(Y) , parent_of(X, Y).
+mother_of(X, Y) :- female(Y) , parent_of(X, Y).
+
+grandfather_of(X, Y) :- parent_of(X, Z) , father_of(Z, Y).
+grandmother_of(X, Y) :- parent_of(X, Z) , mother_of(Z, Y).
