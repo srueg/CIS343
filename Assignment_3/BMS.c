45d138af0ab2b65bc5cadfd9484286e881fe097d
Simon Rüegg <simon@rueggs.ch>
2016-10-03 17:30:53 -0400

    Initial setup for C Coding 2

---
+++ Assignment_3/BMS.c
@@ -0,0 +1,18 @@
+/** 
+    Author: Simon Rüegg
+    Assignment: C Coding 2
+    DuaDate: Monday, October 10, 2016
+**/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+int main(){
+
+    return EXIT_SUCCESS;
+}
+
+
