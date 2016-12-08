bc904c38bac33aad2132b822ce0dcb4d3a6fc9bc
Simon Rüegg <simon@rueggs.ch>
2016-12-08 14:49:19 -0500

    ex1

---
+++ doctor/doctor.rkt
@@ -0,0 +1,93 @@
+#lang racket
+(define (visit-doctor name) 
+   ( (write (list 'hello name)) 
+    (write '(what seems to be the trouble?)) 
+    (doctor-driver-loop name)))
+ 
+(define doctor-driver-loop
+         (lambda (name)
+           ((display "\n")
+           (display '**)
+           (let ((user-response (read)))
+             (cond
+                 ( (equal? user-response '(goodbye))
+                      (write (list 'goodbye name))
+                )
+                 (else
+                  (go-on name user-response )
+                  ))))))
+         
+(define go-on
+  (lambda (name user-response)
+((write (reply user-response))
+                  (doctor-driver-loop name))))
+ 
+(define (reply user-response) 
+    (cond ((fifty-fifty)
+        (append (qualifier) 
+        (change-person user-response))) 
+    (else (hedge))))
+
+(define (fifty-fifty) (= (random 2) 0))
+
+ (define (qualifier) 
+    (pick-random '
+    ((you seem to think) 
+     (you feel that) 
+     (why do you believe) 
+     (why do you say)
+
+     (why do you feel that)
+     (what do you feel when)
+     (tell me more about)
+     (so you think that)
+     (apparently you think)
+    ))) 
+ 
+(define (hedge) 
+    (pick-random '
+    ((please go on) 
+     (many people have the same sorts of feelings) 
+     (many of my patients have told me the same thing) 
+     (please continue)
+     
+     (interesting go on)
+     (I understand how you feel)
+     (tell me more)
+     (you're not the only one with these feelings)
+     (you'll feel better if you talk about it)
+    )))
+
+(define (replace pattern replacement lst) 
+    (cond ((null? lst) '()) 
+        ((equal? (car lst) pattern) 
+        (cons replacement 
+        (replace pattern replacement (cdr lst)))) 
+    (else 
+        (cons (car lst) 
+    (replace pattern replacement (cdr lst))))))
+
+(define (many-replace replacement-pairs lst) 
+    (cond ((null? replacement-pairs) lst) 
+    (else 
+        (let ((pat-rep (car replacement-pairs))) 
+        (replace (car pat-rep)
+        (cadr pat-rep)
+    (many-replace (cdr replacement-pairs) lst))))))
+
+(define (change-person phrase)
+    (many-replace '((i you) (me you) (am are) (my your))
+    phrase) )
+
+(define (pick-random lst)
+    (nth (+ 0 (random (length lst) ) ) lst) )
+
+(define (nth n lst)
+   (cond
+   ((empty? lst) null)
+   ((= n 0)
+   (car lst))
+  (else
+    (nth (- n 1) (cdr lst)))))
+
+(visit-doctor 'simon)
